9e0fbab13112d209f1b88f88109256fe333b954b
JaeHoo Bae <juniorking@naver.com>
2018-04-08 12:58:31 +0900

    폴더 구조 변경

---
+++ example_game/Assets/02_Scripts/Editor/BundleBuilder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.IO;
+
+public class BundleBuilder : MonoBehaviour
+{
+    [MenuItem("Assets/Build AssetBundle")]
+    public static void BuildAssetBundle()
+    {
+        string path = Application.dataPath + "/AssetBundles";
+        
+
+
+        BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.Android);
+    }
+}
